9101644b8433e3ced37817a26898bccfb66d7847
Manoj472 <manu96041@gmail.com>
2024-08-02 10:30:35 +0530

    changes

---
+++ directory/Javascript/methods/String_Methods.js
@@ -0,0 +1,45 @@
+let a="lakshmimanoj'manoj"
+console.log(a.length)
+console.log(a.charAt(7))
+console.log(a.charCodeAt(9))
+console.log(a.at(7))
+console.log(a[7])
+console.log(a.slice(7,12))
+console.log(a.substring(7,12))
+console.log(a.substr(7,4))
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+let first="             lakshmi"
+let last="manoj              "
+let fullname=first.concat(last)
+console.log(first.trimStart())
+console.log(last.trimEnd())
+console.log(fullname)
+let b="5";
+console.log(b.padStart(5,"."))
+console.log(b.padEnd(5,"."))
+console.log(b.repeat(5))
+console.log(a.replace("lakshmi","M."))
+console.log(a.indexOf("a"))
+console.log(a.lastIndexOf("m"))
+console.log(a.search(/manoj/))
+let c = "The rain in SPAIN stays mainly in the plain";
+console.log(c.match(/ain/g))
+console.log(c.match(/ain/gi))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
